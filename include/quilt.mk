be7000/openwrt_repo

include: add support to apply patches from openwrt-patches
Signed-off-by: Ermine Jose <quic_erminejo@quicinc.com>
Change-Id: Ica6e62a27a10254d956a6e8b8f02c22c299894eb

include/quilt.mk
@@ -34,6 +34,7 @@ endif
 ifneq ($(if $(DUMP),1,$(__quilt_inc)),1)
 __quilt_inc:=1
 
+FindPackage?=$(strip $(shell find $(TOPDIR)/openwrt-patches -name $(1) 2>/dev/null))
 PATCH_DIR?=$(CURDIR)/patches
 FILES_DIR?=$(CURDIR)/files
 HOST_PATCH_DIR?=$(PATCH_DIR)
@@ -87,6 +88,7 @@ endef
 define Build/Patch/Default
 	$(if $(QUILT),rm -rf $(PKG_BUILD_DIR)/patches; mkdir -p $(PKG_BUILD_DIR)/patches)
 	$(call PatchDir,$(PKG_BUILD_DIR),$(PATCH_DIR),)
+	$(call PatchDir, $(PKG_BUILD_DIR),$(call FindPackage, $(basename $(notdir $(CURDIR))))/patches,)
 	$(if $(QUILT),touch $(PKG_BUILD_DIR)/.quilt_used)
 endef
 

CodeLinaro-mirror/qsdk_oss_prpl-foundation_prplos_prplos

(cherry picked from commit 42d3517fb1a3a428ad625a1ad7d9ff74759e073f)
(cherry picked from commit 235f20574fa3781d33e5faa99ba70e2e1577e52c)

include/quilt.mk
@@ -34,6 +34,7 @@ endif
 ifneq ($(if $(DUMP),1,$(__quilt_inc)),1)
 __quilt_inc:=1
 
+FindPackage?=$(strip $(shell find $(TOPDIR)/prpl-patches -name $(1) 2>/dev/null))
 PATCH_DIR?=$(CURDIR)/patches
 FILES_DIR?=$(CURDIR)/files
 HOST_PATCH_DIR?=$(PATCH_DIR)
@@ -87,6 +88,7 @@ endef
 define Build/Patch/Default
 	$(if $(QUILT),rm -rf $(PKG_BUILD_DIR)/patches; mkdir -p $(PKG_BUILD_DIR)/patches)
 	$(call PatchDir,$(PKG_BUILD_DIR),$(PATCH_DIR),)
+	$(call PatchDir, $(PKG_BUILD_DIR),$(call FindPackage, $(basename $(notdir $(CURDIR))))/patches,)
 	$(if $(QUILT),touch $(PKG_BUILD_DIR)/.quilt_used)
 endef
 